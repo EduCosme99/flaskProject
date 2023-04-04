814461501ca36939a6a7e20041d03aa6982520a4
José Cosme <al74139@utad.eu>
2023-04-04 21:54:01 +0100

    Página 1  - Logo Funcional  - Botões
     Página 2
     - Password

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/login')
 def pag_inicial():
     return render_template('PagInicial.html')
 
+
+@app.route('/geral')
+def login_geral():
+    return render_template('LoginGeral.html')
 
 if __name__ == '__main__':
     app.run()
