421831613fb03608b28a025aac50b59dc31fabb8
José Cosme <al74139@utad.eu>
2023-04-07 22:04:54 +0100

    Página 1 (Página Inicial de Login)  - Logo Funcional  - Botões
     Página 2 (Login Geral)
     - Password
    
     Página 3 (Login Supervisores)
     - Email
     - Password
    
     Página 4 (Inicial Supervisores)
    
     *Base dados iniciada

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -11,24 +11,57 @@ class Funcionario(db.Model):
     nome = db.Column(db.String(length=256), nullable=False)
     email = db.Column(db.String(length=256), nullable=True, unique=True)
     password = db.Column(db.String(length=30), nullable=False)
-    posto = db.Column(db.Integer(), nullable=False)
-    linha = db.Column(db.Integer(), nullable=False)
+    id_posto = db.Column(db.Integer(), db.ForeignKey('posto.id'), nullable=False)
+    posto = db.relationship('Posto', backref='funcionarios')
+    id_linha = db.Column(db.Integer(), db.ForeignKey('linha.id'), nullable=False)
+    linha = db.relationship('Linha', backref='funcionarios')
     id_turno = db.Column(db.Integer(), db.ForeignKey('turno.id'), nullable=False)
     turno = db.relationship('Turno', backref='funcionarios')
     id_cargo = db.Column(db.Integer(), db.ForeignKey('cargo.id'), nullable=False)
     cargo = db.relationship('Cargo', backref='funcionarios')
 
 
+class Cargo(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    descricao_cargo = db.Column(db.String(length=100), nullable=False, unique=True)
+
+
 class Checklist(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     ckl_inicio = db.Column(db.DateTime(), nullable=False)
     ckl_fim = db.Column(db.DateTime(), nullable=False)
-    aprovacao = db.Column(db.Boolean(),nullable=False)
+    aprovacao = db.Column(db.Boolean(), nullable=False)
+    em_andamento = db.Column(db.Boolean(), nullable=False, default=False)
+    id_funcionario = db.Column(db.Integer(), db.ForeignKey('funcionario.id'), nullable=False)
+    funcionario = db.relationship('Funcionario', backref='checklists')
+    id_turno = db.Column(db.Integer(), db.ForeignKey('turno.id'), nullable=False)
+    turno = db.relationship('Turno', backref='checklists')
 
 
-class Cargo(db.Model):
+class Parametro(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    descricao_cargo = db.Column(db.String(length=100), nullable=False, unique=True)
+    descricao = db.Column(db.String(length=256), nullable=False, unique=True)
+
+
+class Linha(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    descricao = db.Column(db.String(length=256), nullable=False, unique=True)
+
+
+class Verificacao(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    descricao = db.Column(db.String(length=45), nullable=False)
+    horario = db.Column(db.DateTime(), nullable=False)
+    id_parametro = db.Column(db.Integer(), db.ForeignKey('parametro.id'), nullable=False)
+    parametro = db.relationship('Parametro', backref='verificacoes')
+    id_checklist = db.Column(db.Integer(), db.ForeignKey('checklist.id'), nullable=False)
+    checklist = db.relationship('Checklist', backref='verificacoes')
+
+
+class Posto(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    id_linha = db.Column(db.Integer(), db.ForeignKey('linha.id'), nullable=False)
+    linha = db.relationship('Linha', backref='postos')
 
 
 class Turno(db.Model):
